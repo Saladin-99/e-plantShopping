dc688c5587787b8eadd43f55fd0fff847870c4b7
Salaheldin Ashraf <salahabdou99@gmail.com>
2025-06-24 19:06:39 -0400

    add alert when adding item to cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -263,6 +263,7 @@ function ProductList({ onHomeClick }) {
           ...prevState, // Spread the previous state to retain existing entries
           [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
         }));
+        alert('Product added to cart successfully!');
       };
 
     return (
